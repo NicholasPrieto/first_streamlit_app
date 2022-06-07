7823ae72a0abfdaae213fa3847957b413353f653
NicholasPrieto <103974069+NicholasPrieto@users.noreply.github.com>
2022-06-07 08:21:52 -0500

    Update streamlit_app.py
    Imported Requests package, connected to fruityvice api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit
 import pandas
+#New Section to display fruityvice api response
+Streamlit.header ('Fruityvice Fruit Advice!)
+                  
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
 
 streamlit.title("My Mom's New Healthy Diner")
 
